d0e4eb970aa805ee55c310f3abb0ba941428222b
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-09 20:24:36 -0300

    feat: it creates global style files and configure theme provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import { router } from "./routes";
 
+import { colors, fonts, gaps, Reset } from "@/styles";
+
+const theme = { colors, fonts, gaps };
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Reset />
+
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
