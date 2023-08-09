c1f4758e3e067e220e3d7e436c1e9376ea1dfd3e
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-09 20:25:43 -0300

    feat: it updates Product screen

--- web/src/screens/Product/Product.tsx
+++ web/src/screens/Product/Product.tsx
@@ -1,5 +1,11 @@
+import { Header } from "@/components";
+
 const Product = () => {
-  return <h1>Product</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Product;
