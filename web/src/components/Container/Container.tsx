746804a0b7ad1c46b1e0af417faa5d744c4162d9
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-10 11:12:30 -0300

    feat: Container component updated
    breakpoints added

--- web/src/components/Container/Container.tsx
+++ web/src/components/Container/Container.tsx
@@ -1,19 +1,31 @@
-import { FC, ReactNode } from "react";
-
-import styled from "styled-components";
-
-interface ContainerProps {
-  children: ReactNode;
-}
-
-const ContainerComponent: FC<ContainerProps> = ({ children }) => {
-  return <Container>{children}</Container>;
-};
+import styled, { css } from "styled-components";
 
 const Container = styled.section`
-  max-width: 1180px;
-  width: 100%;
-  margin: 0 auto;
+  ${({ theme }) => css`
+    width: 100%;
+    max-width: 300px;
+    margin: 0 auto;
+
+    @media (min-width: ${theme.breakpoints.sm}) {
+      max-width: 500px;
+    }
+
+    @media (min-width: ${theme.breakpoints.md}) {
+      max-width: 720px;
+    }
+
+    @media (min-width: ${theme.breakpoints.lg}) {
+      max-width: 920px;
+    }
+
+    @media (min-width: ${theme.breakpoints.xl}) {
+      max-width: 1180px;
+    }
+
+    @media (min-width: ${theme.breakpoints.xxl}) {
+      max-width: 1380px;
+    }
+  `}
 `;
 
-export default ContainerComponent;
+export default Container;
--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Container } from "./Container";
 export { default as Header } from "./Header";
+export { default as Text } from "./Text";
+export { default as Title } from "./Title";
