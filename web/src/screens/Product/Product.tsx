3d23fd7f4c1c1eaf2c7f5d226306948fd737d7a8
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-10 19:41:03 -0300

    feat: Product screen updated

--- web/src/screens/Product/Product.tsx
+++ web/src/screens/Product/Product.tsx
@@ -1,11 +1,53 @@
-import { Header } from "@/components";
+import { styled } from "styled-components";
+
+import { Container, Header, ProductSelector } from "@/components";
+
+import { PIZZAS_IMAGES_LIST } from "@/constants";
 
 const Product = () => {
+  const pizzaImages = () =>
+    PIZZAS_IMAGES_LIST.map(({ img2x, alt }) => (
+      <PizzaImage src={img2x} alt={alt} />
+    ));
+
   return (
     <div>
       <Header />
+
+      <SelectedPizzaSection>
+        <SelectedPizzaContainer>
+          <PizzaImages>{pizzaImages()}</PizzaImages>
+
+          <ProductSelector />
+        </SelectedPizzaContainer>
+      </SelectedPizzaSection>
     </div>
   );
 };
+
+const SelectedPizzaSection = styled.section`
+  margin-top: 16px;
+  margin-bottom: 32px;
+`;
+
+const SelectedPizzaContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 7fr 5fr;
+  align-items: start;
+`;
+
+const PizzaImages = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 24px;
+`;
+
+const PizzaImage = styled.img`
+  max-width: 360px;
+  max-height: 360px;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+`;
 
 export default Product;
