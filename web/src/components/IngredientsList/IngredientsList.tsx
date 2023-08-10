738fc86dcb3c3b300b6cb32912b34ef3647ecf5d
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-10 19:37:15 -0300

    feat: IngredientsList component added

---
+++ web/src/components/IngredientsList/IngredientsList.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import { styled } from "styled-components";
+import { MdDone } from "react-icons/md";
+
+import { Text } from "@/components";
+import { IngredientOption } from "@/interfaces";
+
+import { theme } from "@/styles";
+
+interface IngredientsListProps {
+  options: IngredientOption[];
+}
+
+const IngredientsList: FC<IngredientsListProps> = ({ options }) => {
+  const ingredientsOptions = () =>
+    options.map(({ description }) => (
+      <Ingredient key={description}>
+        <MdDone />
+        <Text as="li" fontSize="12px" color={theme.colors.gray["900"]}>
+          {description}
+        </Text>
+      </Ingredient>
+    ));
+
+  return <Ingredients>{ingredientsOptions()}</Ingredients>;
+};
+
+const Ingredients = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
+
+const Ingredient = styled.li`
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+
+  & > svg {
+    width: 28px;
+    height: 22px;
+    fill: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export default IngredientsList;
