75a75b5fd5ea5af3c9addb63dc04e9a6655468fb
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-10 19:39:25 -0300

    feat: Title component updated

--- web/src/components/Title/Title.tsx
+++ web/src/components/Title/Title.tsx
@@ -2,14 +2,14 @@ import { TitleProps } from "@/interfaces";
 import { css, styled } from "styled-components";
 
 const Title = styled.h2<TitleProps>`
-  ${({ theme, color, fontWeight, fontSize, textAlign, letterSpacing }) => css`
+  ${({ theme, color, fontWeight, fontSize, align, letterSpacing }) => css`
     font-family: ${theme.fonts.primary};
     font-size: ${fontSize ?? "32px"};
     font-weight: ${fontWeight ?? "700"};
     line-height: 1em;
     margin-top: 1px;
     color: ${color ?? theme.colors.gray["900"]};
-    text-align: ${textAlign ?? "left"};
+    text-align: ${align ?? "left"};
     letter-spacing: ${letterSpacing ?? "none"};
   `}
 `;
