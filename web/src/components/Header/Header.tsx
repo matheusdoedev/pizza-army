08eb9be62e004e636657fe1b1d28fa7f8e5e6c69
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-10 11:12:06 -0300

    feat: Header component updated

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,29 +1,131 @@
-import { FaChevronDown, FaUser, FaShoppingCart } from "react-icons/fa";
+import { styled } from "styled-components";
+import { FaChevronDown } from "react-icons/fa";
+import { MdOutlineShoppingCart, MdOutlinePerson, MdMenu } from "react-icons/md";
 
-import { Container } from "@/components";
+import { Container, Text, Title } from "@/components";
+
+import { theme } from "@/styles";
 
 const Header = () => {
   return (
     <header>
-      <Container>
-        <section>
-          <button>
-            Comprar <FaChevronDown />
-          </button>
-        </section>
+      <HeaderContainer>
+        <BuyButton>
+          <Text fontWeight="600" letterSpacing="0.21px">
+            Comprar
+          </Text>{" "}
+          <FaChevronDown />
+        </BuyButton>
 
-        <h1>Pizza Armly</h1>
+        <BurguerMenuButton>
+          <MdMenu />
+        </BurguerMenuButton>
 
-        <section>
-          <p>A segunda pizza com 20% OFF</p>
-          <FaUser />
+        <Title
+          as="h1"
+          fontSize="24px"
+          color={theme.colors.primary}
+          textAlign="center"
+          letterSpacing="1.92px"
+        >
+          Pizza Army
+        </Title>
+
+        <HeaderInfoSection>
+          <DiscountText
+            letterSpacing="0.21px"
+            textAlign="right"
+            fontWeight="600"
+            color={theme.colors.gray["800"]}
+          >
+            A segunda pizza com <strong>20% OFF</strong>
+          </DiscountText>
+
+          <UserButton>
+            <MdOutlinePerson />
+          </UserButton>
+
           <button>
-            <FaShoppingCart />
+            <MdOutlineShoppingCart />
           </button>
-        </section>
-      </Container>
+        </HeaderInfoSection>
+      </HeaderContainer>
     </header>
   );
 };
+
+const HeaderContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  align-items: center;
+  padding: 12px 0;
+
+  @media (min-width: ${theme.breakpoints.lg}) {
+    grid-template-columns: repeat(3, 1fr);
+    padding: 18px 0;
+  }
+`;
+
+const BuyButton = styled.button`
+  display: none;
+
+  @media (min-width: ${theme.breakpoints.lg}) {
+    display: flex;
+    align-items: center;
+    column-gap: 6px;
+
+    & > svg {
+      width: 10px;
+      height: 10px;
+    }
+  }
+`;
+
+const HeaderInfoSection = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  column-gap: 24px;
+
+  & svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const DiscountText = styled(Text)`
+  display: none;
+
+  @media (min-width: ${theme.breakpoints.lg}) {
+    display: block;
+
+    & > strong {
+      color: ${theme.colors.primary};
+    }
+  }
+`;
+
+const UserButton = styled.button`
+  display: none;
+
+  @media (min-width: ${theme.breakpoints.lg}) {
+    display: block;
+  }
+`;
+
+const BurguerMenuButton = styled.button`
+  display: flex;
+
+  & > svg {
+    position: relative;
+    left: -8px;
+    width: 36px;
+    height: 24px;
+  }
+
+  @media (min-width: ${theme.breakpoints.lg}) {
+    display: none;
+  }
+`;
 
 export default Header;
