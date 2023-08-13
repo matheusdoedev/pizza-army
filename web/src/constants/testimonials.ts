fb12643ce4c6e429b180bcac6950c40acab04649
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-13 09:50:41 -0300

    feat: it add ids to testimonials const array

--- web/src/constants/testimonials.ts
+++ web/src/constants/testimonials.ts
@@ -1,15 +1,18 @@
 export const TESTIMONIALS_LIST = [
   {
+    id: 1,
     text: "Pizza incrível. Queijo de qualidade, bem recheada e crocante. Vou pedir mais e indicar pra todo mundo, com certeza!",
     userName: "Pedro B.",
     initialAnimationTranslateYPosition: "-90px",
   },
   {
+    id: 2,
     text: "Pizza incrível. Queijo de qualidade, bem recheada e crocante. Vou pedir mais e indicar pra todo mundo, com certeza!",
     userName: "Pedro B.",
     initialAnimationTranslateYPosition: "-60px",
   },
   {
+    id: 3,
     text: "Pizza incrível. Queijo de qualidade, bem recheada e crocante. Vou pedir mais e indicar pra todo mundo, com certeza!",
     userName: "Pedro B.",
     initialAnimationTranslateYPosition: "-30px",
