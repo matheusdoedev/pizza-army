5670abc02c60055aa48b9a35f965f200a82c8ac1
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-14 20:49:52 -0300

    feat(web): it creates Toast component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 
+import { Toast } from "@/components";
 import { queryClient } from "@/providers";
 import { router } from "@/routes";
 
 import { Animations, Reset, theme } from "@/styles";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
@@ -15,6 +18,7 @@ const App = () => {
         <Animations />
 
         <RouterProvider router={router} />
+        <Toast />
       </ThemeProvider>
     </QueryClientProvider>
   );
